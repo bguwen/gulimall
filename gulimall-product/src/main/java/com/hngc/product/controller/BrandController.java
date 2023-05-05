90ab5f62efd93735ec1d435b8482a909cf3397fa
hnitbw <hn@hnitbw>
2023-05-05 14:08:49 +0800

    完成BrandController 查询所有品牌--根据id修改品牌--根据id批量删除品牌--添加品牌--接口

--- gulimall-product/src/main/java/com/hngc/product/controller/BrandController.java
+++ gulimall-product/src/main/java/com/hngc/product/controller/BrandController.java
@@ -1,8 +1,13 @@
 package com.hngc.product.controller;
 
+import com.common.utils.Result;
+import com.hngc.product.entity.Brand;
+import com.hngc.product.service.BrandService;
 import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * <p>
@@ -17,5 +22,52 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/brand")
 public class BrandController {
 
+    @Autowired
+    private BrandService brandService;
+
+    /**
+     * 查询所有品牌
+     *
+     * @return
+     */
+    @GetMapping("list")
+    public Result list() {
+        return Result.success().put("data", brandService.list());
+    }
+
+    /**
+     * 根据id修改品牌
+     *
+     * @param brand
+     * @return
+     */
+    @PostMapping("update")
+    public Result updateBrand(@RequestBody Brand brand) {
+        brandService.updateById(brand);
+        return Result.success();
+    }
+
+    /**
+     * 根据id批量删除品牌
+     *
+     * @param ids
+     * @return
+     */
+    @DeleteMapping("delete")
+    public Result deleteBrand(List<Long> ids) {
+        brandService.removeByIds(ids);
+        return Result.success();
+    }
+    /**
+     * 添加品牌
+     *
+     * @param brand
+     * @return
+     */
+    @PostMapping("add")
+    public Result addBrand(@RequestBody Brand brand) {
+        brandService.save(brand);
+        return Result.success();
+    }
 
 }
